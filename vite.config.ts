9c2a56d472cc05624281b67fbcb2d934f50fad32
Vicente Plata <xientyto@gmail.com>
2026-03-20 16:11:54 -0600

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/tiny-creatures/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
